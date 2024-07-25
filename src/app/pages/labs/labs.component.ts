754a97654a50b31a0ab29eab5f122cb77f7a1188
Pablo <pablogarcirodriguez@gmail.com>
2024-07-25 18:59:46 +0200

    Property Binding en Angular

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
 @Component({
   selector: 'app-labs',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './labs.component.html',
-  styleUrl: './labs.component.css'
+  styleUrl: './labs.component.css',
 })
 export class LabsComponent {
   welcome = 'Bienvenido a mi primera aplicación con Angular';
-  tasks = [
-    'Instalar Angular CLI',
-    'Crear proyecto',
-    'Crear componentes'
-  ]
+  tasks = ['Instalar Angular CLI', 'Crear proyecto', 'Crear componentes'];
+  name = 'Pablo';
+  age = 35;
+  disabled = true;
+  image = 'https://www.w3schools.com/howto/img_avatar.png';
+  person = {
+    name: 'Pablo',
+    age: 35,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png',
+  };
 }
