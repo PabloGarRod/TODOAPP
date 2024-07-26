5eff8b22cc6d8e1108c35d7e16d4a5d929edd962
Pablo <pablogarcirodriguez@gmail.com>
2024-07-26 08:32:26 +0200

    Event binding: keydown

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -28,4 +28,9 @@ export class LabsComponent {
   changeHandler(event: Event) {
     console.log(event);
   }
+
+  keydownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
 }
