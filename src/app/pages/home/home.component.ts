4f0838b7d1239a43ab317eab986aca451283ee53
Pablo <pablogarcirodriguez@gmail.com>
2024-07-27 13:19:33 +0200

    Update tasks

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { Task } from '../../models/task.model';
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -9,23 +11,56 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  tasks = signal([
-    'Instalar Angular CLI',
-    'Crear proyecto',
-    'Crear componentes',
-    'Crear servicios',
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Crear proyecto',
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: 'Crear componentes',
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: 'Crear servicios',
+      completed: false,
+    },
   ]);
 
   changeHandler(event: Event) {
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
+    this.addTask(newTask);
+  }
+
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
     this.tasks.update((tasks) => [...tasks, newTask]);
   }
 
   deleteTask(index: number) {
-    // this.tasks.update((tasks) => tasks.filter((_, i) => i !== index));
     this.tasks.update((tasks) =>
       tasks.filter((task, position) => position !== index)
     );
+  }
+
+  updateTask(index: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            completed: !task.completed,
+          };
+        }
+        return task;
+      });
+    });
   }
 }
